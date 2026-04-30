2a1fd4e08053626f2e750ecfbd2354723b97f784
Lia S <lia800225@gmail.com>
2026-04-30 13:00:19 +0200

    interfejs historii

---
+++ lab8_Games_WP/HistoryWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace lab8_Games_WP;
+
+public partial class HistoryWindow : Window
+{
+    public HistoryWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void BtnZamknij_Click(object source, RoutedEventArgs args)
+    {
+        this.Close(); 
+    }
+}
