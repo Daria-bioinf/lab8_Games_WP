a68e8d0f65b8f4e699ebf3a6d207ee54e11b27eb
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-04-30 11:01:22 +0200

    game1_logika

--- lab8_Games_WP/MainWindow.axaml.cs
+++ lab8_Games_WP/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System;
 
 namespace lab8_Games_WP
 {
@@ -10,16 +11,57 @@ namespace lab8_Games_WP
             InitializeComponent();
         }
 
+        Random rand = new Random();
+        int currentCard = 0;
+        int score = 0;
         public void BtnStart_Click(object sender, RoutedEventArgs e)
         {
+            currentCard = rand.Next(1, 11);
+            score = 0;
+
+            txtCurrentCard.Text = currentCard.ToString();
+            txtScore.Text = "Punkty: 0";
+            txtResult.Text = "Zgadnij następną kartę!";
         }
 
         public void BtnWieksza_Click(object sender, RoutedEventArgs e)
         {
+            int newCard = rand.Next(1, 11);
+
+            if (newCard > currentCard)
+            {
+                score++;
+                txtResult.Text = "Dobrze!";
+            }
+            else
+            {
+                txtResult.Text = "Źle! Koniec gry";
+                return;
+            }
+
+            currentCard = newCard;
+            txtCurrentCard.Text = currentCard.ToString();
+            txtScore.Text = $"Punkty: {score}";
         }
 
         public void BtnMniejsza_Click(object sender, RoutedEventArgs e)
         {
+            int newCard = rand.Next(1, 11);
+
+            if (newCard < currentCard)
+            {
+                score++;
+                txtResult.Text = "Dobrze!";
+            }
+            else
+            {
+                txtResult.Text = "Źle! Koniec gry";
+                return;
+            }
+
+            currentCard = newCard;
+            txtCurrentCard.Text = currentCard.ToString();
+            txtScore.Text = $"Punkty: {score}";
         }
     }
 }
