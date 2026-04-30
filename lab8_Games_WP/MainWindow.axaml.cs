68399d479ef9f3ff8c2e772492830812bdb9640c
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-04-30 10:04:55 +0200

    Add project files.

---
+++ lab8_Games_WP/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace lab8_Games_WP
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
