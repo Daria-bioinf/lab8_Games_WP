94fa239bbc8a4a2f47c8c854c4919028fd61aa23
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-04-30 10:28:57 +0200

    class gameService

---
+++ lab8_Games_WP/GameService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_Games_WP
+{
+    internal class GameService
+    {
+    }
+}
