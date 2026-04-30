d63eb19453801e430c9ae64465e5ad3576ccfb80
Lia S <lia800225@gmail.com>
2026-04-30 11:04:51 +0200

    menu z wyborem gier

---
+++ lab8_Games_WP/MenuWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace lab8_Games_WP;
+
+public partial class Window1 : Window
+{
+    public Window1()
+    {
+        InitializeComponent();
+    }
+
+    public void BtnDodajGracza_Click(object source, RoutedEventArgs args)
+    {
+       
+    }
+
+    public void BtnGra1_Click(object source, RoutedEventArgs args)
+    {
+        
+    }
+
+    public void BtnGra2_Click(object source, RoutedEventArgs args)
+    {
+     
+    }
+
+    public void BtnGra3_Click(object source, RoutedEventArgs args)
+    {
+       
+    }
+
+    public void BtnHistoria_Click(object source, RoutedEventArgs args)
+    {
+       
+    }
+}
