1c25726f513b20c4019e831511c64707afa740ea
Daria Piskovtseva <d.piskovtseva@gmail.com>
2026-05-05 13:30:28 +0200

    05.05

--- lab8_Games_WP/LiarGameWindow.axaml.cs
+++ lab8_Games_WP/LiarGameWindow.axaml.cs
@@ -2,11 +2,18 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace lab8_Games_WP;
 
 public partial class LiarGameWindow : Window
 {
+    Random rand = new Random();
+    int realCard = 0;
+    int claimedCard = 0;
+    int score = 0;
+    bool isLying = false;
+
     public LiarGameWindow()
     {
         InitializeComponent();
@@ -28,6 +35,25 @@ public partial class LiarGameWindow : Window
 
     public void BtnNext_Click(object source, RoutedEventArgs args)
     {
-        
+        realCard = rand.Next(1, 11);
+
+        isLying = rand.Next(0, 2) == 0;
+
+        if (isLying)
+        {
+            do
+            {
+                claimedCard = rand.Next(1, 11);
+            } while (claimedCard == realCard);
+        }
+        else
+        {
+            claimedCard = realCard;
+        }
+        txtClaim.Text = $"Komputer mowi: To jest {claimedCard}";
+        txtCardValue.Text = "?";
+        txtStatus.Text = "Czy on klamie?";
+
+        btnNext.IsEnabled = false;
     }
 }
--- lab8_Games_WP/Window1.axaml.cs
+++ lab8_Games_WP/Window1.axaml.cs
@@ -1,12 +1,11 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
 
 namespace lab8_Games_WP;
 
 public partial class Window1 : Window
 {
+    string currentPLayer = "";
     public Window1()
     {
         InitializeComponent();
@@ -14,12 +13,27 @@ public partial class Window1 : Window
 
     public void BtnDodajGracza_Click(object source, RoutedEventArgs args)
     {
-       
+        currentPLayer = txtLogin.Text ?? "";
+
+        if (string.IsNullOrEmpty(currentPLayer))
+        {
+            txtObecnyGracz.Text = "Wpisz login!";
+            return;
+        }
+        txtObecnyGracz.Text = $"obecny gracz: {currentPLayer}";
     }
 
     public void BtnGra1_Click(object source, RoutedEventArgs args)
     {
-        
+        if (string.IsNullOrEmpty(currentPLayer))
+        {
+            txtObecnyGracz.Text = "Najpierw dodaj gracza!";
+            return;
+        }
+        var gameWindow = new MainWindow();
+        gameWindow.Show();
+
+        this.Close();
     }
 
     public void BtnGra2_Click(object source, RoutedEventArgs args)
@@ -30,13 +44,13 @@ public partial class Window1 : Window
 
     public void BtnGra3_Click(object source, RoutedEventArgs args)
     {
-       
+
     }
 
     public void BtnHistoria_Click(object source, RoutedEventArgs args)
     {
-       
+
     }
 
-    
+
 }
